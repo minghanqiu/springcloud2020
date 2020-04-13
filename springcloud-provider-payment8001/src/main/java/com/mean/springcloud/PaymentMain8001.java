b784380739c684c7470f038fe9ac24a53ce153fe
zhengjia <mnb1235438>
2020-04-13 23:15:20 +0800

    提交支付模块代码

---
+++ springcloud-provider-payment8001/src/main/java/com/mean/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.mean.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/***
+ *  @Author :  ZhengJia
+ *  Date:  2020/4/13
+ *  Time: 20:53
+ *  Description:
+ **/
+@SpringBootApplication
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
